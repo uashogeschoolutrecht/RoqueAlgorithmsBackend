322ef70964f298e6a7ae3356a8418468c22757f5
LibrechtKuijvenhoven <librecht@live.nl>
2023-01-23 11:40:00 +0100

    added version to similarity for concurrency

---
+++ FakeNewsBackend/Migrations/Similarity/20230118092715_added-version.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FakeNewsBackend.Migrations.Similarity
+{
+    public partial class addedversion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                schema: "public",
+                table: "Similarities",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                schema: "public",
+                table: "Similarities");
+        }
+    }
+}
